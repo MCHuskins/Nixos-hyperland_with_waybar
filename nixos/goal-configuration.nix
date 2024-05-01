ddfddf7cd95ab84dce32c0f75bc348eb8cc34b3c
MCHuskins <38637043+MCHuskins@users.noreply.github.com>
2024-05-01 13:43:36 -0700

    add all part by parts to rule file
    I think I am getting clsoe to it fuckign workign

--- nixos/goal-configuration.nix
+++ nixos/goal-configuration.nix
@@ -1,108 +1,17 @@
- # Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, inputs,  ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    #  ./lanzaboote.nix
-    ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # 
-
   # Change runtime directory size
  # services.logind.extraConfig = "RuntimeDirectorySize=8G";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-# Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-#nix flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-#plasma
-services.xserver.displayManager.sddm.enable = true;
-services.xserver.desktopManager.plasma5.enable = true;
- 
-# hyperland
-  programs.hyprland.enable = true;
-  programs.hyprland.package = inputs.hyprland.packages."${pkgs.system}".hyprland;
 #might remove 
 #  environment.sessionVariables.NIXOS_OZONE_WL = "1";
 #  environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-
-
-
-systemd.services.foo = {
-  script = ''
-    echo 76 > /sys/class/power_supply/BAT0/charge_control_end_threshold
-  '';
-  wantedBy = [ "multi-user.target" ];
-};
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-    # Fonts
-fonts.packages = with pkgs; [
-    jetbrains-mono
-    nerd-font-patcher
-  ];
 
 #screen thing 
 #  services.geoclue2.appConfig = {
 #      "gammastep" = {
 #        isAllowed = true;
 #        isSystem = false;
-#        users = [ "1000" ]; # FIXME: set your user id (to get user id use command 'id -u "your_user_name"')
+#        users = [ "1000" ]; # (it is 1000 for me) set your user id (to get user id use command 'id -u "your_user_name"')
 #      };
 #  };  
  # programs.light.enable = true;
@@ -166,13 +75,6 @@ fonts.packages = with pkgs; [
 
 
   environment.systemPackages = with pkgs; [
-     vim
-     sbctl 
-     niv
-     lshw
-     kitty
-     firefox
-     vscode
     #terminal-utils
      zoxide
     #  fzf
@@ -251,39 +153,4 @@ fonts.packages = with pkgs; [
     driSupport = true;
     driSupport32Bit = true;
   };
-
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.lin = {
-    isNormalUser = true;
-    description = "lin";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [  
-    ];
-  };
-
-
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-  hardware.nvidia.prime = {
-    sync.enable = true;
-		# offload = {
-			# enable = true;
-			# enableOffloadCmd = true;
-		# };
-		# Make sure to use the correct Bus ID values for your system!
-		intelBusId = "PCI:0:2:0";
-		nvidiaBusId = "PCI:01:0:0";
-	};
-
-
 }
